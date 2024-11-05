134a47634dcd8b74fa935da5e7d667445ba5c21c
Abhey Rana <abheyrana3114@gmail.com>
2024-11-05 12:06:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function call(city) {
-    let API_Key = "a5292d1b891fdc9c4eadd80fbdfd5cee";
+    let API_Key = "5833b36c07ec07b9f016037a9ec3a4ee";
     cityName.innerHTML = '<div class="text-center"> <div class="spinner-border" role = "status" ><span class="visually-hidden">Loading...</span></div ></div > '
     
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}&units=metric`)
